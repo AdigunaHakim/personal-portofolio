91b9e487a2e7dc054253bef9998fd8b7b4ab44fd
“AdigunaSuryaHakim” <adiguna.hakim@btpn.com>
2021-03-28 22:50:49 +0700

    initiate portofolio

---
+++ src/components/Social.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class Social extends Component {
+    render() {
+        return (
+            <div class="social">
+            <a href="https://github.com/naaficodes" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://Instagram.com/adigunahakim" target="_blank" rel="noopener noreferrer"><i class="fab fa-instagram"></i></a>
+            <a href="https://www.facebook.com/adigunasuryahakim" target="_blank" rel="noopener noreferrer"><i class="fab fa-facebook-f"></i></a>
+            <a href="https://www.linkedin.com/in/adiguna-surya-hakim-741357154/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
+            <a href="https://medium.com/@iadiguna_hakim" target="_blank" rel="noopener noreferrer"><i class="fab fa-medium-m"></i></a>
+            </div>
+            )
+        }
+    }
+    
+export default Social
+    
---
+++ src/contents/About.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div className="condiv">
+            <h1 className="subtopic">About Me</h1>
+            <h4>Hey there,</h4>
+            <h1>I'm Adiguna Surya Hakim</h1>
+            <h3>Senior Software Engineer | Fullstack Developer | <u>Technology Enthusiast</u></h3>
+            <br></br>
+            <p>I’m a tech triple threat - database architecture, software & telecommunication engineer. Enthusiastic software engineer with 5+ years experience of system development, especially backend by prioritizing security, maintainability & scalability. My greatest strength is the ability of good analysis, in designing system architecture & problem solving. 
+            </p>
+            
+            </div>
+            )
+        }
+    }
+    
+export default About
+    
