123e68704dfe97a915cacb7493494f2a1b0c76b0
“AdigunaSuryaHakim” <adiguna.hakim@btpn.com>
2021-03-28 23:15:14 +0700

    add experience contents

--- src/contents/Education.js
+++ src/contents/Education.js
@@ -1,17 +1,26 @@
-import React, { Component } from 'react';
-import Widecard from '../components/Widecard';
+import React, { Component } from "react";
+import Widecard from "../components/Widecard";
 
 class Education extends Component {
-    render() {
-        return (
-            <div className="condiv">
-            <h1 className="subtopic">My Education</h1>
-            <Widecard title="Teknik Informatika" where="Politeknik Elektronika Negeri Surabaya" from="2014" to="2016"/>
-            <Widecard title="Manajemen Informatika" where="Politeknik Negeri Jember" from="2010" to="2013"/>
-            </div>
-            )
-        }
-    }
-    
-export default Education
-    
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">My Education</h1>
+        <Widecard
+          title="Teknik Informatika"
+          where="Politeknik Elektronika Negeri Surabaya"
+          from="2014"
+          to="2016"
+        />
+        <Widecard
+          title="Manajemen Informatika"
+          where="Politeknik Negeri Jember"
+          from="2010"
+          to="2013"
+        />
+      </div>
+    );
+  }
+}
+
+export default Education;
---
+++ src/contents/Experience.js
@@ -0,0 +1,44 @@
+import React, { Component } from "react";
+import Widecard from "../components/Widecard";
+
+class Experience extends Component {
+  render() {
+    return (
+      <div className="condiv">
+        <h1 className="subtopic">My Experience</h1>
+        <Widecard
+          title="Technical Lead of IT Common Service Enablement"
+          where="PT Bank Tabungan Pensiun Nasional | Jakarta, Indonesia"
+          from="Aug 2019"
+          to="Present"
+        />
+        <Widecard
+          title="Application Development Engineer | Fullstack Developer"
+          where="PT Bank Tabungan Pensiun Nasional | Jakarta, Indonesia"
+          from="Feb 2019"
+          to="Aug 2019"
+        />
+        <Widecard
+          title="Software Engineer | Backend Developer"
+          where="PT Aero System Indonesia | Tangerang, Indonesia"
+          from="Mar 2018"
+          to="Feb 2019"
+        />
+        <Widecard
+          title="Radio Network Optimization Engineer | 4G/4.5G"
+          where="PT Telkominfra | Surabaya, Indonesia"
+          from="Feb 2017"
+          to="Mar 2018"
+        />
+        <Widecard
+          title="Operation Support Engineer"
+          where="PT Lintas Media Telekomunikasi | Jakarta, Indonesia"
+          from="Jan 2016"
+          to="Jan 2017"
+        />
+      </div>
+    );
+  }
+}
+
+export default Experience;
