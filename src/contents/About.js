9bd895b4966c7b836e24c49600c54b423c76e82f
“AdigunaSuryaHakim” <adiguna.hakim@btpn.com>
2021-03-29 15:18:18 +0700

    fix contact & about

--- src/contents/About.js
+++ src/contents/About.js
@@ -11,12 +11,17 @@ class About extends Component {
         </h3>
         <br></br>
         <p>
-          I’m a tech triple threat - database architecture, software &
-          telecommunication engineer. Enthusiastic software engineer with 5+
+          I’m a tech triple threat - database architecture, telecomunication
+          engineer & fullstack developer. Enthusiastic software engineer with 5+
           years experience of system development, especially backend by
           prioritizing security, maintainability & scalability. My greatest
           strength is the ability of good analysis, in designing system
           architecture & problem solving.
+        </p>
+        <p>
+          I'm very interested in the topic of machine learning, big data & web
+          scrapping. I am very enthusiastic about exploring java, javasciprt,
+          go, python and its frameworks.
         </p>
       </div>
     );
